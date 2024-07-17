183f9458322158b2f3d1205498f176c592829808
Mateus Ataide <teteulima@outlook.com>
2024-07-17 14:32:34 -0300

    feat/FirstPage: finished design of the main page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@/components/button';
 import { Input } from '@/components/input';
 import { colors } from '@/styles/colors';
-import { Calendar as IconCalendar, MapPin, Settings2 } from 'lucide-react-native';
+import { ArrowRight, Calendar as IconCalendar, MapPin, Settings2, UserRoundPlus } from 'lucide-react-native';
 import { Image, Text, View } from 'react-native';
 
 
@@ -15,12 +15,12 @@ export default function Home() {
             <View className='w-full bg-zinc-900 p-4 rounded-xl my-8 border border-zinc-800'>
                 <Input>
                     <MapPin color={colors.zinc[400]} size={20}/>
-                    <Input.Field placeholder='para onde?'/>
+                    <Input.Field placeholder='Para onde?'/>
                 </Input>
 
                 <Input>
                     <IconCalendar color={colors.zinc[400]} size={20}/>
-                    <Input.Field placeholder='para onde?'/>
+                    <Input.Field placeholder='Quando?'/>
                 </Input>
 
                 <View className='py-3 border-b border-zinc-800'>
@@ -30,8 +30,20 @@ export default function Home() {
                     </Button>
                 </View>
 
-                
+                <Input>
+                    <UserRoundPlus color={colors.zinc[400]} size={20}/>
+                    <Input.Field placeholder='Quem estará na viagem?'/>
+                </Input>
+
+                <Button >
+                        <Button.Title>Continuar</Button.Title>
+                        <ArrowRight color={colors.lime[950]} size={20}/>
+                    </Button>
             </View>
+
+            <Text className='text-zinc-500 font-regular text-base text-center'>
+                Ao planejar sua viagem pela plann.er você automaticamente concorda com os nossos{" "}<Text className='text-zinc-200 underline'>termos de uso e politicas de privacidade</Text>
+            </Text>
 
         </View>
     )
